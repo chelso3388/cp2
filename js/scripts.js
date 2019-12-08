fc8bd8b0ebff478ceb22e4f7b22cbd44724b438e
Chelsea L Gifford <chelsealgifford@Chelseas-MacBook-Pro.local>
2019-12-07 20:01:23 -0800

    add new branching to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function check (){
+  var q1 = document.quiz.q1.value;
+  var q2 = document.quiz.q2.value;
+  var q3 = document.quiz.q3.value;
+  var q4 = document.quiz.q4.value;
+  var q5 = document.quiz.q5.value;
+  var correct = 0;
+}
