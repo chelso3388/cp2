cc323f6e3ed4e108efb422e78446091535005859
Chelsea L Gifford <chelsealgifford@Chelseas-MacBook-Pro.local>
2019-12-07 21:11:52 -0800

    add messages to correct answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 function check (){
   var q1 = document.quiz.q1.value;
   var q2 = document.quiz.q2.value;
@@ -22,7 +23,24 @@ function check (){
     if (q5 =="c2") {
       correct++;
     }
+var messages = ["JavaScript", "Ruby", "C#"];
+
+var range;
+  if (correct < 1) {
+    range = 0;
+  }
+
+  if (correct > 0 && correct < 3) {
+    range = 1;
+  }
+
+  if (correct > 2) {
+    range = 2;
+  }
+
 document.getElementById("after_submit").style.visibility = "visible";
-document.getElementById("number_correct").innerHTML = "You got" + correct + "correct";
+
+document.getElementById("message").innerHTML = messages[range];
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct";
 
 }
